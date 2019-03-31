2801751f25b9fb1ed4037fa2106a273c3e70404d
Jake Bartlow <jtbartlow@gmail.com>
2019-03-31 21:08:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,8 @@ end
 
 def get_japanese_emoticon(file_path, eng_emoticon)
   library = load_library(file_path)
-  libray["get_emoticon"].include?(eng_emoticon)
+  library["get_emoticon"].include?(eng_emoticon)
+  library["get_emoticon"][eng_emoticon]
 end
 
 def get_english_meaning
